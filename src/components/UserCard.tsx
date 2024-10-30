01a17fed36122b3b058be6fb6be05ec23d4a26d3
Jason Chen <jasonschen3@gmail.com.>
2024-10-30 14:00:52 -0400

    Finish up to step 4

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -3,51 +3,138 @@ import {
   Flex,
   HStack,
   Text,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  Link,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
+import { apiUrl, Service } from "@hex-labs/core";
+import axios from "axios";
 
 type Props = {
   user: any;
 };
 
-
-// TODO: right now, the UserCard only displays the user's name and email. Create a new modal component <UserModal> that
-// pops up when the card is clicked. In this modal, list all the user's information including name, email, phoneNumber,
-// and userId. 
-
-// TODO: Explore if you can display the email as a link to the user's email that will open up the user's 
-// email client and start a new email to that user. Also explore if you can provide a link to the user's resume.
-
-// TODO: In our database structure, every user has a userId that is unique to them. This is the primary key of the user
-// and is referenced in their applications to all of our hexathons. Create a button that when clicked, will retrieve all of
-// the hexathons that the user has applied to. You can use the /applications endpoint of the registration service to do this
-// and the /hexathons endpoint of the hexathons service to get a list of all the hexathons.
-
 const UserCard: React.FC<Props> = (props: Props) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [hexathons, setHexathons] = useState<any[]>([]);
+
+  // TODO DOESN'T WORK YET
+  // Function to fetch hexathons based on user applications
+  const fetchUserHexathons = async () => {
+    try {
+      console.log(props.user.name.first, props.user.id);
+
+      // 1) Fetch all hexatons
+      const hexathonsRes = await axios.get(
+        apiUrl(Service.HEXATHONS, "/hexathons")
+      );
+      console.log("All hexathons", hexathonsRes.data);
+      const allHexathons = hexathonsRes.data;
+      // 2) Loop thru all hackathons
+      const userHexathons = await Promise.all(
+        allHexathons.map(async (hexathon: any) => {
+          const hexathonId = hexathon.id;
+
+          // Query applications by hexathonId
+          const appRes = await axios.get(
+            apiUrl(Service.REGISTRATION, "/applications"),
+            { params: { hexathon: hexathonId, userId: props.user.id } }
+          );
+
+          console.log(`Applications for hexathon ${hexathonId}:`, appRes.data);
+
+          // If user has an application for this hexathon, return it
+          if (appRes.data.length > 0) {
+            return hexathon;
+          }
+        })
+      );
+      // TODO
+    } catch (error) {
+      console.error("Error fetching user's hexathons", error);
+    }
+  };
 
   return (
-    <Box
-    borderWidth="1px"
-    rounded="lg"
-    boxShadow="lg"
-    height="175px"
-    fontWeight="bold"
-    alignItems="center"
-    >
-      <Flex padding="2" flexDirection="column">
-        <HStack align="flex-end" justify="space-between">
-          <Text fontSize='xl'>{`${props.user.name.first} ${props.user.name.last}`}</Text>
-        </HStack>
-        <Text
-          fontSize="sm"
-          fontWeight="semibold"
-          justifyContent="justify"
-          mt="2"
-        >
-          {props.user.email}
-        </Text>
-      </Flex>
-    </Box>
+    <>
+      <Box
+        borderWidth="1px"
+        rounded="lg"
+        boxShadow="lg"
+        height="175px"
+        fontWeight="bold"
+        alignItems="center"
+        onClick={onOpen}
+        cursor="pointer"
+      >
+        <Flex padding="2" flexDirection="column">
+          <HStack align="flex-end" justify="space-between">
+            <Text fontSize="xl">{`${props.user.name.first} ${props.user.name.last}`}</Text>
+          </HStack>
+          <Text
+            fontSize="sm"
+            fontWeight="semibold"
+            justifyContent="justify"
+            mt="2"
+          >
+            {props.user.email}
+          </Text>
+        </Flex>
+      </Box>
+
+      {/* Modal */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{`${props.user.name.first} ${props.user.name.middle ? props.user.name.middle + " " : ""} ${props.user.name.last}`}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>
+              <strong>Email:</strong>{" "}
+              <Link href={`mailto:${props.user.email}`} color="blue.500">
+                {props.user.email}
+              </Link>
+            </Text>
+            <Text>
+              <strong>Phone:</strong> {props.user.phoneNumber}
+            </Text>
+            <Text>
+              <strong>User ID:</strong> {props.user.userId}
+            </Text>
+
+            {/* Display hexathons the user applied to */}
+            {hexathons.length > 0 && (
+              <>
+                <Text mt={4} fontWeight="bold">
+                  Hexathons Applied:
+                </Text>
+                <ul>
+                  {hexathons.map((hexathon) => (
+                    <li key={hexathon.id}>{hexathon.name}</li>
+                  ))}
+                </ul>
+              </>
+            )}
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="teal" onClick={fetchUserHexathons}>
+              Show Applied Hexathons
+            </Button>
+            <Button onClick={onClose} ml={3}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
